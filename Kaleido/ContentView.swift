cbeb02a5e1bb4d60cd1cc053577e15fcecdd1d0d
L Ryholt <46486518+lucaryholt@users.noreply.github.com>
2020-09-04 15:39:01 +0200

    Update ContentView.swift
    small refactor

--- Kaleido/ContentView.swift
+++ Kaleido/ContentView.swift
@@ -17,7 +17,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            LucaView14()
+            V14()
             V16()
             V20()
             V22()
